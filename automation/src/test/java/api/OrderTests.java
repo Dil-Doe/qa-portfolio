f678145322d8598692003002b04c0dbfe0d55db6
Dil-Doe <ruby03@list.ru>
2026-09-01 18:45:08 +0300

    Create OrderTests.java

---
+++ automation/src/test/java/api/OrderTests.java
@@ -0,0 +1,60 @@
+package api;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+class OrderTests {
+
+    @Test
+    void shouldCreateOrder() {
+        RestAssured.baseURI = "https://dummyjson.com";
+
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "userId": 1,
+                          "products": [
+                            {
+                              "id": 1,
+                              "quantity": 2
+                            }
+                          ]
+                        }
+                        """)
+                .when()
+                .post("/carts/add")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("userId", equalTo(1))
+                .body("products[0].id", equalTo(1))
+                .body("products[0].quantity", equalTo(2));
+    }
+
+    @Test
+    void shouldRejectInvalidProductId() {
+        RestAssured.baseURI = "https://dummyjson.com";
+
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "userId": 1,
+                          "products": [
+                            {
+                              "id": 999999,
+                              "quantity": 1
+                            }
+                          ]
+                        }
+                        """)
+                .when()
+                .post("/carts/add")
+                .then()
+                .statusCode(404);
+    }
+}
