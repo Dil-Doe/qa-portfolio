6ee956e51dbf5bd05de04ffa0e49f9f4532941a0
Dil-Doe <ruby03@list.ru>
2026-09-01 18:50:19 +0300

    Create LoginPage.java

---
+++ automation/src/test/java/ui/automation/src/test/java/ui/LoginPage.java
@@ -0,0 +1,20 @@
+package ui;
+
+public class LoginPage {
+
+    public void open() {
+    }
+
+    public void enterEmail(String email) {
+    }
+
+    public void enterPassword(String password) {
+    }
+
+    public void clickLogin() {
+    }
+
+    public boolean isLoginSuccessful() {
+        return true;
+    }
+}
